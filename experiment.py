c6c1606cf08fe065c4aef41b40a9fea42477c959
liam-j-simon <liamjsimon@hotmail.com>
2020-04-07 23:39:40 +1000

    fix

--- experiment.py
+++ experiment.py
@@ -1,7 +1,10 @@
+import faulthandler; faulthandler.enable()
 import ijson
 import string
 from mpi4py import MPI
 import os
+import io
+
 
 #mpi variables
 comm = MPI.COMM_WORLD
@@ -10,27 +13,25 @@ size = comm.size
 rank = comm.rank
 
 
-#f_name = "data/tinyTwitter.json"
+f_name = "data/tinyTwitter.json"
 #f_name = "data/smallTwitter.json"
-f_name = "data/bigTwitter.json"
+#f_name = "data/bigTwitter.json"
 
 
-
+#open file
+f = open(f_name, "rb")
 
 
 ####################################
-#use No. of processes to split lines in file 
+#use No. of processes to split lines in file
 ####################################
 if rank == 0:
-    #open file
-    f = open(f_name,"r")
     #read file length
     f_size = os.path.getsize(f_name)
     print(f_size)
     #t_size doesn't include master
     t_size = size-1
     last_lines = 0
-    t_size = 3
     duration = f_size/t_size
     splits = [[int(i * duration),int((i+1) * duration)] for i in range(t_size)]
     print(splits)
@@ -41,11 +42,10 @@ if rank == 0:
             break
         f.seek(end)
         f.readline()
-        splits[i][1] = f.tell() -1
-        splits[i+1][0] = f.tell() 
+        splits[i][1] = f.tell() 
+        splits[i+1][0] = f.tell()
     print(splits)
     print("###########################")
-    f.close()
     for j,chunk in enumerate(splits,1):
         comm.send(chunk,dest=j)
     '''
@@ -61,20 +61,20 @@ if rank == 0:
                 set(h_result) | set(result[0])}
         l_result = {key: l_result.get(key,0) + result[1].get(key,0) for key in
                 set(l_result) | set(result[1])}
+    h_sorted = {x: y for x,y in sorted(h_result.items(), key = lambda item:
+        item[1],reverse=True)[0:10]}
+    l_sorted = {x: y for x,y in sorted(l_result.items(), key = lambda item:
+        item[1],reverse=True)[:10]}
 
-    h_sorted = dict(sorted(h_result.iteritems(), key=operator.itemgetter(1),
-    reverse=True)[:10]
-    l_sorted = dict(sorted(l_result.iteritems(), key=operator.itemgetter(1),
-    reverse=True)[:10]
-    print(h_sorted_
-    print(l_sorted)
+    for i,(name,count) in enumerate(h_sorted.items(),1):
+        print("{}. {},{}".format(i,name,count))
 
+    for i,(name,count) in enumerate(l_sorted.items(),1):
+        print("{}. {},{}".format(i,name,count))
 
 ####################################
 else:
     ################
-    #open file
-    f = open(f_name,"r")
     chunk = comm.recv(source=0)
     start = chunk[0]
     end = chunk[1]
@@ -83,13 +83,17 @@ else:
     hashtags = dict()
     languages = dict()
     f.seek(start)
+    print(rank, "this is the start", start, f.tell())
     parser = ijson.parse(f)
     try:
         #find each part of json in stream
-        print(rank,f.tell(),start,end)
-        while f.tell() <= end:
-            print(f.tell(),rank)
-            for prefix,event,value in parser:
+        print("breaks after this",f.tell(),rank)
+        f_size = os.path.getsize(f_name)
+        print(f_size)
+        for prefix,event,value in parser:
+            if rank == 2:
+             print(rank, f.tell(),end,(f.tell()<end))
+            if f.tell() < end:
                 #check for language
                 if prefix == "rows.item.doc.metadata.iso_language_code":
                         #add 1 if already in dict
@@ -107,13 +111,15 @@ else:
                             hashtags[value] = hashtags[value]+1
                         else:
                             hashtags[value] = 1
+            else:
+                break
     #catch exception
     except Exception as e:
-        print("")
+        print(e)
 
     comm.send([hashtags,languages],dest=0)
     #print(rank,hashtags)
     #print(rank,languages)
 
-            
+
 
